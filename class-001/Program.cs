8be4e2934418c4e90e5834719d89e7f910faf5b2
monteirorudinei <monteiro.rudinei@hotmai.com>
2020-11-09 18:42:14 -0300

    segunda linha

--- class-001/Program.cs
+++ class-001/Program.cs
@@ -7,6 +7,7 @@ namespace class_001
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
