7deb3ee2d801c4a46ddf46bb1a03069c8a07cb58
monteirorudinei <monteiro.rudinei@hotmai.com>
2020-11-11 16:24:55 -0300

    novo campo CreateDate

--- class-001/User.cs
+++ class-001/User.cs
@@ -1,5 +1,8 @@
 class User
 {
+    private date createDate;
+
     public int UserID { get; set; }
     public string Name { get; set; }
+    public date CreateDate { get => createDate; set => createDate = value; }
 }
